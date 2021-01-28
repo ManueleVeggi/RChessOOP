643d83f8495d7ad63573584e14441409cf80e15d
ManueleVeggi <48963689+ManueleVeggi@users.noreply.github.com>
2021-01-28 01:06:59 +0100

    Class Rook is added

--- LABProject.R
+++ LABProject.R
@@ -101,3 +101,80 @@ King <- setRefClass("King",
                         return(FALSE)
                       }
                     ))
+
+Rook <- setRefClass("Rook",
+                    fields=list(
+                      color = "character", #  w - white, b - black
+                      isRook = "logical"
+                    ),
+                    methods=list(
+                      initialize = function(newColor) {
+                        #  We want to check if the newColor passed as parameter 
+                        #  is 'w' or 'b'; If the value is not valid, the 
+                        #  execution of the software is stopped
+                        if ((newColor != "w") && (newColor != "b")) {
+                          stop("invalid parameters to the ctor (", newColor, 
+                               " but only 'b' and 'w' are valid)")
+                        } else {
+                          color <<- newColor
+                          isRook <<- FALSE
+                        }
+                      },
+                      checkTrajectory = function(src, dest, currentColor, cb) {
+                        cat("Here I am, in checkTrajectory!")
+                        swapped<-FALSE
+                        if ((src$col == dest$col) && (abs(src$row-dest$row)>1))  {
+                          if (src$row > dest$row) {
+                            temp <- src$row
+                            src$row <- dest$row
+                            dest$row <- temp
+                            swapped <- TRUE
+                          }
+                          for (i in seq (src$row+1, dest$row-1)) {
+                            if (!(is.null(cb[i, src$col][[1]]))) {
+                              cat("Trajectory 1\n")
+                              return(FALSE)}}
+                          if(swapped) { 
+                            #  re-swapping
+                            temp <- src$row
+                            src$row <- dest$row
+                            dest$row <- temp
+                          }}
+                        else if ((src$row==dest$row) && (abs(src$col-dest$col)>1)) { 
+                          if (src$col > dest$col) {
+                            temp <- src$col
+                            src$col <- dest$col
+                            dest$col <- temp
+                            swapped <- TRUE
+                          }
+                          for (i in seq (src$col + 1, dest$col - 1)) {
+                            if (!(is.null(cb[src$row, i][[1]]))) {
+                              cat("Trajectory 2\n")
+                              return(FALSE)}}
+                          if(swapped) { 
+                            #   re-swapping
+                            temp <- src$col
+                            src$col <- dest$col
+                            dest$col <- temp
+                          }
+                        }
+                        cat("checktrajectory is true")
+                        cat("a: ", src$row)
+                        cat("b: ", src$col)
+                        cat("c: ", dest$row)
+                        cat("d: ", dest$col)
+                        return (TRUE)
+                      },
+                      #  defines the move this piece can do
+                      checkMove=function(source,destination, cb, currentColor) {
+                        cat("Here I am, in checkMove!")
+                        if (((abs(destination$row-source$row) <= 8) && (destination$col == source$col)) || 
+                            (((destination$row == source$row)) && ((abs(destination$col-source$col) <= 8 )))) {
+                          #  valid move
+                          return(TRUE)
+                        }
+                        #  invalid move
+                        cat("Move not allowed\n")
+                        return(FALSE)
+                      }
+                    ))
